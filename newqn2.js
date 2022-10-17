517eae71672b38ce336c4f638b4f425103920765
Abisheik <77277634+abisheik88@users.noreply.github.com>
2022-10-17 10:36:24 +0530

    Update newqn2.js

--- newqn2.js
+++ newqn2.js
@@ -6,3 +6,17 @@ const result = [];
 for(let i = 0;i < Q;i++)
    result.push(Math.min(...Numbers.slice(UVInputs[i][0], UVInputs[i][1])));
 console.log(result);
+
+
+const N = userInput[0];
+const Q = userInput[1];
+const Numbers = userInput[2].split(" ");
+var a = userInput[3].split(" ");
+var b = userInput[4].split(" ");
+var c = userInput[5].split(" ")
+const UVInputs = [a, b, c];
+const result = [];
+for(let i = 0;i < Q;i++){
+   result.push(Math.min(...Numbers.slice(UVInputs[i][0], UVInputs[i][1])));
+   }
+console.log(result.join(" \n"));
